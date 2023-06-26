f8aa575309cea78295583a4ae137d0f15576afea
Michael Santoso <michaelsantoso40@gmail.com>
2023-06-26 16:39:21 +0800

    Added home page and fixed some css

--- src/home/HomeMain.tsx
+++ src/home/HomeMain.tsx
@@ -1,7 +1,13 @@
+import "./home.css";
+import study from "./study.jpg";
 function HomeMain() {
   return (
-    <div>
-      <div>Welcome to Productisite!</div>
+    <div className="wrapper">
+      <img src={study} className="study"></img>
+      <div className="title">Welcome to Productisite!</div>
+      <div className="body">
+        Built to increase your productivity. Feel free to explore the web.
+      </div>
     </div>
   );
 }
