939e4a7b442035884a9f74bb3bdf70656fb73344
Michael Santoso <michaelsantoso40@gmail.com>
2023-06-29 04:40:03 +0800

    Added the css for the weather forecast to make it more professional

--- src/Weather/WeatherMain.tsx
+++ src/Weather/WeatherMain.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
+import "./weather.css";
+import search from "./search.svg";
 
 function WeatherMain() {
   const [city, setCity] = useState("");
@@ -35,6 +37,7 @@ function WeatherMain() {
         setLocation(resp.data.location);
         setTemperature(resp.data.current);
         setIcon(resp.data.current.condition.icon);
+        setError("");
         console.log("API was called");
         console.log(location);
       })
@@ -45,32 +48,50 @@ function WeatherMain() {
   };
 
   return (
-    <div>
-      <p>Find out the weather in:</p>
-      <input
-        value={city}
-        onKeyUp={handleKeyPress}
-        onChange={cityInput}
-        placeholder="Search for city"
-      />
-      <button onClick={city ? () => callWeatherAPI(city) : undefined}>
-        Submit
-      </button>
-      {location && (
-        <p>{`Location: ${location?.name ?? "N/A"}, ${
-          location?.country ?? "N/A"
-        }`}</p>
-      )}
-      {temperature && (
-        <div>
-          <p>{`Temperature: ${temperature?.temp_c} °C`}</p>
-          <p>{`Wind: ${temperature?.wind_kph} kph`}</p>
-          <p>{`Humidity: ${temperature?.humidity} %`}</p>
-          <p>{`Pressure: ${temperature?.pressure_mb} mb`}</p>
+    <div className="container">
+      <div className="weather-search">
+        <div className="search-submit">
+          <input
+            value={city}
+            onKeyUp={handleKeyPress}
+            onChange={cityInput}
+            placeholder="Search for city"
+            className="search-instruction input-field"
+          />
+          <button
+            onClick={city ? () => callWeatherAPI(city) : undefined}
+            className="search-button"
+          >
+            <img src={search} className="search"></img>
+          </button>
         </div>
-      )}
-      <img src={icon}></img>
-      {error && <p>{error}</p>}
+      </div>
+      <div className="wrapper-weather-card">
+        <div className={temperature ? "weather-card" : "weather-card-empty"}>
+          {location && (
+            <p className="location">{`${location?.name ?? "N/A"}, ${
+              location?.country ?? "N/A"
+            }`}</p>
+          )}
+          {temperature && (
+            <div className="celcius">
+              <p>{`${temperature?.temp_c.toFixed(0)}°C`}</p>
+            </div>
+          )}
+          {temperature && (
+            <div className="temperature">
+              <p>{`Temperature: ${temperature?.temp_c} °C`}</p>
+              <p>{`Wind: ${temperature?.wind_kph} kph`}</p>
+              <p>{`Humidity: ${temperature?.humidity} %`}</p>
+              <p>{`Pressure: ${temperature?.pressure_mb} mb`}</p>
+            </div>
+          )}
+          <div className="icon-container">
+            <img src={icon} className="icon"></img>
+          </div>
+          {error && <p>{error}</p>}
+        </div>
+      </div>
     </div>
   );
 }
